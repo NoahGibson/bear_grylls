0e796c37b0293776be1e0048e2b8fba8cf1a17d1
akauntz <anastasiakauntz@gmail.com>
2020-01-30 16:34:24 -0500

    file organization

--- bear_grylls/lib/camera.dart
+++ bear_grylls/lib/camera.dart
@@ -1,4 +1,4 @@
-//either description or image
+//Opens camera. Output: image
 
 import 'package:flutter/material.dart';
 import 'package:bear_grylls/facts.dart';
@@ -9,7 +9,28 @@ import 'package:flutter/material.dart';
 import 'package:path/path.dart' show join;
 import 'package:path_provider/path_provider.dart';
 
-void main() => runApp(MyApp());
+//void main() => runApp(MyApp());
+Future<void> main() async {
+  // Ensure that plugin services are initialized so that `availableCameras()`
+  // can be called before `runApp()`
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Obtain a list of the available cameras on the device.
+  final cameras = await availableCameras();
+
+  // Get a specific camera from the list of available cameras.
+  final firstCamera = cameras.first;
+
+  runApp(
+    MaterialApp(
+      theme: ThemeData.dark(),
+      home:TakePictureScreen (
+        // Pass the appropriate camera to the TakePictureScreen widget.
+        camera: firstCamera,
+      ),
+    ),
+  );
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -29,7 +50,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: BearFacts(),
     );
   }
 }
@@ -119,6 +140,128 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
+// A screen that allows users to take a picture using a given camera.
+class TakePictureScreen extends StatefulWidget {
+  final CameraDescription camera;
+
+  const TakePictureScreen({
+    Key key,
+    @required this.camera,
+  }) : super(key: key);
+
+  @override
+  TakePictureScreenState createState() => TakePictureScreenState();
+}
+
+class TakePictureScreenState extends State<TakePictureScreen> {
+  CameraController _controller;
+  Future<void> _initializeControllerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    // To display the current output from the Camera,
+    // create a CameraController.
+    _controller = CameraController(
+      // Get a specific camera from the list of available cameras.
+      widget.camera,
+      // Define the resolution to use.
+      ResolutionPreset.medium,
+    );
+
+    // Next, initialize the controller. This returns a Future.
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    // Dispose of the controller when the widget is disposed.
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Take a picture')),
+      // Wait until the controller is initialized before displaying the
+      // camera preview. Use a FutureBuilder to display a loading spinner
+      // until the controller has finished initializing.
+      body: FutureBuilder<void>(
+        future: _initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            // If the Future is complete, display the preview.
+            return CameraPreview(_controller);
+          } else {
+            // Otherwise, display a loading indicator.
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.camera_alt),
+        // Provide an onPressed callback.
+        onPressed: () async {
+          // Take the Picture in a try / catch block. If anything goes wrong,
+          // catch the error.
+          try {
+            // Ensure that the camera is initialized.
+            await _initializeControllerFuture;
+
+            // Construct the path where the image should be saved using the
+            // pattern package.
+            final path = join(
+              // Store the picture in the temp directory.
+              // Find the temp directory using the `path_provider` plugin.
+              (await getTemporaryDirectory()).path,
+              '${DateTime.now()}.png',
+            );
+
+            // Attempt to take a picture and log where it's been saved.
+            await _controller.takePicture(path);
+
+            // If the picture was taken, display it on a new screen.
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => DisplayPictureScreen(imagePath: path),
+              ),
+            );
+          } catch (e) {
+            // If an error occurs, log the error to the console.
+            print(e);
+          }
+        },
+      ),
+    );
+  }
+}
+
+// A widget that displays the picture taken by the user.
+class DisplayPictureScreen extends StatelessWidget {
+  final String imagePath;
+
+  const DisplayPictureScreen({Key key, this.imagePath}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Display the Picture')),
+      // The image is stored as a file on the device. Use the `Image.file`
+      // constructor with the given path to display the image.
+      //body: Image.file(File(imagePath)),
+    body: Center(
+      child:  Image.asset(
+        'Images/BEAR.jpg',
+        width: 600,
+      height: 240,
+    )
+    )
+    );
+  }
+}
+
 
 class FirstRoute extends StatelessWidget {
   @override
@@ -132,8 +275,8 @@ class FirstRoute extends StatelessWidget {
           child: Text('Take a Picture'),
           onPressed: () {
             Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => SecondRoute()),
+                context,
+                MaterialPageRoute(builder: (context) => SecondRoute()),
             );
           },
         ),
@@ -150,19 +293,19 @@ class SecondRoute extends StatelessWidget {
         title: Text("Polar Bear!"),
       ),
       body: Center(
-          child: RaisedButton(
-            //onPressed: () {
-            //   Navigator.pop(context);
-            //},
-            //child: Text('Go back!'),
-            //)
-            child:Image.asset(
-              'Images/BEAR.jpg',
-              width: 600,
-              height: 500,
-              //fit: BoxFit.cover,
-            ),
-          )
+        child: RaisedButton(
+          //onPressed: () {
+           //   Navigator.pop(context);
+          //},
+          //child: Text('Go back!'),
+        //)
+          child:Image.asset(
+          'Images/BEAR.jpg',
+          width: 600,
+          height: 500,
+          //fit: BoxFit.cover,
+          ),
+        )
       ),
     );
   }
